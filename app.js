c4f69233fe53a297e5b143223fdfedca403d9f58
perca1994 <116674175+perca1994@users.noreply.github.com>
2022-10-27 13:39:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let state= document.getElementById("estate")
+let Tanky= document.getElementById("Thanky")
+function sacarState(){
+    state.style.display="none"; 
+    Tanky.style.display="block";
+};
+
+
